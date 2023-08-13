ee0393228bf6673461526d8d66b57e7cf3e3c2f0
lickx <sinesurfer@gmail.com>
2023-08-13 18:25:50 +0000

    Safe perms for abandoned and reclaimed land

--- OpenSim/Region/CoreModules/World/Land/LandManagementModule.cs
+++ OpenSim/Region/CoreModules/World/Land/LandManagementModule.cs
@@ -1697,7 +1697,9 @@ namespace OpenSim.Region.CoreModules.World.Land
                 land.LandData.OwnerID = m_scene.RegionInfo.EstateSettings.EstateOwner;
                 land.LandData.GroupID = UUID.Zero;
                 land.LandData.IsGroupOwned = false;
-                land.LandData.Flags &= ~(uint) (ParcelFlags.ForSale | ParcelFlags.ForSaleObjects | ParcelFlags.SellParcelObjects | ParcelFlags.ShowDirectory);
+                land.LandData.Flags &= ~(uint) (ParcelFlags.ForSale | ParcelFlags.ForSaleObjects | ParcelFlags.SellParcelObjects | ParcelFlags.ShowDirectory | ParcelFlags.CreateObjects);
+                land.LandData.Name = "Abandoned Land";
+                land.LandData.OtherCleanTime = 5;
 
                 UpdateLandObject(land.LandData.LocalID, land.LandData);
                 m_scene.ForEachClient(SendParcelOverlay);
@@ -1725,7 +1727,9 @@ namespace OpenSim.Region.CoreModules.World.Land
                 land.LandData.SeeAVs = true;
                 land.LandData.AnyAVSounds = true;
                 land.LandData.GroupAVSounds = true;
-                land.LandData.Flags &= ~(uint) (ParcelFlags.ForSale | ParcelFlags.ForSaleObjects | ParcelFlags.SellParcelObjects | ParcelFlags.ShowDirectory);
+                land.LandData.Flags &= ~(uint) (ParcelFlags.ForSale | ParcelFlags.ForSaleObjects | ParcelFlags.SellParcelObjects | ParcelFlags.ShowDirectory | ParcelFlags.CreateObjects);
+                land.LandData.Name = "Reclaimed Land";
+                land.LandData.OtherCleanTime = 5;
                 UpdateLandObject(land.LandData.LocalID, land.LandData);
                 m_scene.ForEachClient(SendParcelOverlay);
                 land.SendLandUpdateToAvatars();
