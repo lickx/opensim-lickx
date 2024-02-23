0e6dc2d9684ea8372349dfd4d69ff619f2ef8694
lickx <sinesurfer@gmail.com>
2024-02-23 05:34:28 +0100

    Change SMTP section to the more descriptive Email

--- OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -519,7 +519,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                     m_useMeshCacheInCastRay = lslConfig.GetBoolean("UseMeshCacheInLlCastRay", m_useMeshCacheInCastRay);
                 }
 
-                IConfig smtpConfig = seConfigSource.Configs["SMTP"];
+                IConfig smtpConfig = seConfigSource.Configs["Email"];
                 if (smtpConfig != null)
                 {
                     // there's an smtp config, so load in the snooze time.
@@ -528,7 +528,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                     m_internalObjectHost = smtpConfig.GetString("internal_object_host", m_internalObjectHost);
                 }
 
-                IConfig chatConfig = seConfigSource.Configs["SMTP"];
+                IConfig chatConfig = seConfigSource.Configs["Chat"];
                 if(chatConfig != null)
                 {
                     m_whisperdistance = chatConfig.GetInt("whisper_distance", m_whisperdistance);
