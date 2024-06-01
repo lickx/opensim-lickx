b97cacc71286ccca359c28f6e9c4ceebafe8f2c6
lickx <sinesurfer@gmail.com>
2024-06-01 15:48:29 +0200

    Add "objectmail_hostname" to llGetEnv()
    It returns the part after the @ for an object's email address

--- OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -6488,6 +6488,11 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                 case "grid":
                     return World.SceneGridInfo == null ? string.Empty : World.SceneGridInfo.GridName;
 
+                case "objectmail_hostname":
+                    if (m_emailModule is not null)
+                        return m_internalObjectHost;
+                    return "";
+
                 default:
                     return "";
             }
