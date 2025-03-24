e4d4ba0d0e7cc4499669dd18d5da6ee2debfa6d5
lickx <11088547+lickx@users.noreply.github.com>
2025-03-24 05:48:46 +0100

    Add opensim.currency support to mkdist

--- mkdist.bat
+++ mkdist.bat
@@ -18,7 +18,7 @@ rem if exist %VERSION_FILE% del %VERSION_FILE%
 
 SET TARGET_ZIP=opensim-%GIT_BRANCH%-%LAST_COMMIT_DATE%_%GIT_REV%.zip
 echo %TARGET_ZIP%
-set excludes=-x "*Tests*" "bin/Gloebit.*" "bin/OpenSimMutelist.Modules.*"
+set excludes=-x "*Tests*" "bin/Gloebit.*" "bin/OpenSimMutelist.Modules.*" "bin/MoneyServer.*" "OpenSim.Data.MySQL.MySQLMoneyDataWrapper.*" "OpenSim.Modules.Currency.dll.*" "OpenSim.ini.sample" "server_cert.p12" "SineWaveCert.pfx"
 zip -r -o "%TARGET_ZIP%" bin CONTRIBUTORS.txt LICENSE.txt README.md ThirdPartyLicenses %excludes%
 
 if not exist bin\OpenSimMutelist.Modules.dll goto skipmutelist
@@ -35,6 +35,13 @@ echo %TARGET_ZIP%
 copy addon-modules\Gloebit\GloebitMoneyModule\Gloebit.ini.example bin\Gloebit.ini.example
 zip -r -o "%TARGET_ZIP%" bin/Gloebit.* %excludes%
 :skipgloebit
+
+if not exist bin\MoneyServer.dll goto skipmoneyserver
+SET TARGET_ZIP=moneyserver-%GIT_BRANCH%-%LAST_COMMIT_DATE%_%GIT_REV%.zip
+set excludes=-x "*Tests*"
+echo %TARGET_ZIP%
+zip -r -o "%TARGET_ZIP%" bin/MoneyServer.* bin/OpenSim.Data.MySQL.MySQLMoneyDataWrapper.* bin/OpenSim.Modules.Currency.* bin/OpenSim.ini.sample bin/server_cert.p12 bin/SineWaveCert.pfx %excludes%
+:skipmoneyserver
 
 
 :end
